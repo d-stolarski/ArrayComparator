4e6f9611743cf0ad1278c4c76d4fc04b7b656279
Daniel Stolarski <d.stolarski@o2.pl>
2017-12-12 14:19:52 +0100

    final commit - some improvements added

--- src/ArrayComparator.java
+++ src/ArrayComparator.java
@@ -1,16 +1,22 @@
 public class ArrayComparator {
-
     public boolean compare(int[][] firstTab, int[][] secondTab) {
-        boolean areEqual = true;
-        for (int i = 0; i < firstTab.length; i++) {
-            if (firstTab.length != secondTab.length || firstTab[i].length != secondTab[i].length) {
-                areEqual = false;
-            } else {
-                for (int j = 0; j < firstTab[i].length; j++) {
-                    if (firstTab[i][j] == secondTab[i][j] && firstTab[i].length == secondTab[j].length) {
-                        continue;
-                    } else {
-                        areEqual = false;
+
+        boolean areEqual = false;
+
+        if (firstTab.length != secondTab.length) {
+            return areEqual;
+        } else {
+            for (int i = 0; i < firstTab.length; i++) {
+                if (firstTab[i].length != secondTab[i].length) {
+                    return areEqual;
+                } else {
+                    for (int j = 0; j < firstTab[i].length; j++) {
+                        if (firstTab[i][j] != secondTab[i][j]) {
+                            areEqual = false;
+                            break;
+                        } else {
+                            areEqual = true;
+                        }
                     }
                 }
             }
